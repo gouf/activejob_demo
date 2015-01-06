b8db95ca76b455fc637a2eeacc91c4c83194c3cb
gouf <innocent.zero@gmail.com>
2015-01-07 05:57:43 +0900

    Set resorce at guest users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'guest_users#index'
 
+
+  resource :guest_users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
